bed0c03d0743af59a888186eb277b82de0e97e62
juaneth <68202118+juaneth@users.noreply.github.com>
2024-06-01 19:40:51 +0100

    did work on project page

--- src/app/project/[project]/page.tsx
+++ src/app/project/[project]/page.tsx
@@ -1,19 +1,49 @@
-import { FaEye } from "react-icons/fa";
-import { Badge } from "~/components/ui/badge";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "~/components/ui/carousel";
 
 export default function Page({ params }: { params: { project: string } }) {
   return (
-    <div>
-      <div className="flex w-full flex-row bg-black p-12">
-        <div>
-          <div className="flex flex-row items-center gap-3">
-            <h1 className="text-3xl font-bold tracking-tight">Argo</h1>
-            <Badge className="flex h-min flex-row gap-2 bg-green-300 hover:bg-green-400">
-              <FaEye className="text-lg"></FaEye>0
-            </Badge>
-          </div>
+    <div className="background flex grow flex-row">
+      <div className="flex w-full flex-col">
+        <div className="bordered-b flex flex-col items-center gap-3 p-12">
+          <h1 className="w-full text-start text-2xl font-semibold tracking-tight">
+            Votes
+          </h1>
+        </div>
 
-          <h2 className="opacity-75">Project Description</h2>
+        <div className="bordered-b flex flex-col items-center gap-3 p-12">
+          <h1 className="w-full text-start text-2xl font-semibold tracking-tight">
+            Fixes
+          </h1>
+        </div>
+      </div>
+
+      <div className="bordered-l bordered-b flex w-[56rem] flex-col items-center gap-5 p-12">
+        <h1 className="w-full text-start text-2xl font-semibold tracking-tight">
+          Images
+        </h1>
+        <div className="w-full px-8">
+          <Carousel
+            opts={{
+              align: "start",
+            }}
+            className=" flex w-full max-w-full"
+          >
+            <CarouselContent>
+              <CarouselItem>
+                <h1>Hi</h1>
+              </CarouselItem>
+              <CarouselItem></CarouselItem>
+              <CarouselItem></CarouselItem>
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
         </div>
       </div>
     </div>
